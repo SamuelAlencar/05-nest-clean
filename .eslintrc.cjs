4413524bc6465af92b85cb0641e2b60fa29d7a5d
samuel_alencar <samuel_alencar@carrefour.com>
2024-01-09 16:58:09 -0300

    fix: adding extra eslint configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,23 @@
-/* eslint-env node */
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    root: true,
-  };
+  env: {
+    // If you don't want to change this to `node: true` globally
+    es2022: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+  "rules": {
+    "@typescript-eslint/no-explicit-any": "error"
+  },
+  overrides: [
+    {
+      files: ['**/*.cjs'],
+      env: {
+        node: true,
+      },
+    },
+  ],      
+};
+
+
--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -1,31 +1,39 @@
-import { Body, ConflictException, Controller, HttpCode, Post } from '@nestjs/common'
+import {
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+} from '@nestjs/common'
 import { PrismaService } from 'src/prisma/prisma.service'
 
 @Controller('/accounts')
 export class CreateAccountController {
-  constructor (private readonly prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
 
   @Post()
   @HttpCode(201)
-  async handle (@Body() body: unknown) {
+  async handle(@Body() body) {
     const { name, email, password } = body
 
     const userWithSameEmail = await this.prisma.user.findUnique({
       where: {
-        email
-      }
+        email,
+      },
     })
 
-    if (userWithSameEmail != null) {
-      throw new ConflictException('User with e-mail address already exists.')
+    if (userWithSameEmail) {
+      throw new ConflictException(
+        'User with same e-mail address already exists.',
+      )
     }
 
     await this.prisma.user.create({
       data: {
         name,
         email,
-        password
-      }
+        password,
+      },
     })
   }
 }
